c624022ba05de9541f5a6a5fcb5344785d730f58
gabrielwahlin <gabriel.p.wahlin@gmail.com>
2025-01-10 11:44:00 +0100

    animations

---
+++ game.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var transition = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	transition.play("fade_in")
